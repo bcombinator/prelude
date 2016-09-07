6aa17408da0395006c0ba9454fd7457bcd605519
David Chase <dannopolsky@freepeople.com>
2016-09-07 00:27:45 -0400

    rebuilt

--- dist/prelude.js
+++ dist/prelude.js
@@ -12,9 +12,6 @@
 		return args.length >= fn.length ? fn.apply(void 0, args) : curry.bind.apply(curry, [ null, fn ].concat( args ));
 	}
 
-	// decode :: String -> String
-	var decode = decodeURIComponent
-
 	// _compose :: (b -> c) -> (a -> b) -> (a -> c)
 	var _compose = function (f, g) { return function () {
 		var args = [], len = arguments.length;
@@ -31,14 +28,11 @@
 		return fns.reduce(_compose);
 	}
 
-	// replace :: Regex -> String -> String -> String
-	var replace = curry(function (pattern, substr, str) { return str.replace(pattern, substr); })
+	// of :: a -> [b]
+	var of = Array.of.bind(Array)
 
-	// toLower :: String -> String
-	var toLower = function (str) { return str.toLowerCase(); }
-
-	// append :: String -> String -> String
-	var append = curry(function (a, b) { return b.concat(a); })
+	// id :: a -> a
+	var id = function (x) { return x; }
 
 	// contains ::  Eq a => a -> [a] -> Bool
 	var contains = curry(function (x, xs) { return xs.indexOf(x) >= 0; })
@@ -55,45 +49,51 @@
 	// join :: String -> [a] -> String
 	var join = curry(function (separator, xs) { return xs.join(separator); })
 
-	// split :: String | RegExp → String → [String]
-	var split = curry(function (separator, str) { return str.split(separator); })
-
 	// map :: (a -> [b]) -> [a] -> [b]
 	var map = curry(function (fn, xs) { return xs.map(fn); })
 
 	// chain :: (a -> b) -> [a] -> [b]
 	var chain = curry(function (fn, xs) { return [].concat.apply([], map(fn, xs)); })
 
-	// of :: a -> [b]
-	var of = Array.of.bind(Array)
-
 	// filter :: (a -> Bool) -> [a] -> [a]
 	var filter = curry(function (pred, xs) { return xs.filter(pred); })
+
+	// decode :: String -> String
+	var decode = decodeURIComponent
+
+	// replace :: Regex -> String -> String -> String
+	var replace = curry(function (pattern, substr, str) { return str.replace(pattern, substr); })
+
+	// toLower :: String -> String
+	var toLower = function (str) { return str.toLowerCase(); }
+
+	// append :: String -> String -> String
+	var append = curry(function (a, b) { return b.concat(a); })
+
+	// split :: String | RegExp → String → [String]
+	var split = curry(function (separator, str) { return str.split(separator); })
 
 	// prepend :: String -> String -> String
 	var prepend = curry(function (a, b) { return a.concat(b); })
 
-	// id :: a -> a
-	var id = function (x) { return x; }
-
 	exports.curry = curry;
-	exports.decode = decode;
 	exports.compose = compose;
-	exports.replace = replace;
-	exports.toLower = toLower;
-	exports.append = append;
+	exports.of = of;
+	exports.id = id;
 	exports.contains = contains;
 	exports.uniq = uniq;
 	exports.concat = concat;
 	exports.union = union;
 	exports.join = join;
-	exports.split = split;
 	exports.map = map;
 	exports.chain = chain;
-	exports.of = of;
 	exports.filter = filter;
+	exports.decode = decode;
+	exports.replace = replace;
+	exports.toLower = toLower;
+	exports.append = append;
+	exports.split = split;
 	exports.prepend = prepend;
-	exports.id = id;
 
 	Object.defineProperty(exports, '__esModule', { value: true });
 
