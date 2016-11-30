1405942a340861f023b9c24ac6fd7e9dd899de93
David Chase <davidchase03@gmail.com>
2016-11-29 23:23:44 -0500

    update _compose to compose2
    -[x] rename _compose to binary compose or compose2
    
    -[x] remove parens better readability imo

--- src/func.js
+++ src/func.js
@@ -1,11 +1,11 @@
 // curry :: ((a, b, c, ...) -> z) -> a -> b -> c -> ... -> z
 export const curry = (fn, ...args) => args.length >= fn.length ? fn(...args) : curry.bind(null, fn, ...args)
 
-// _compose :: (b -> c) -> (a -> b) -> (a -> c)
-const _compose = (f, g) => (...args) => f(g(...args))
+// compose2 :: (b -> c) -> (a -> b) -> a -> c
+const compose2 = (f, g) => x => f(g(x))
 
 // compose :: (a -> c) -> [(a -> a)] -> (a -> c)
-export const compose = (...fns) => fns.reduce(_compose)
+export const compose = (...fns) => fns.reduce(compose2)
 
 // of :: a -> [b]
 export const of = Array.of.bind(Array)
